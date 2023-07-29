38d39fbb0fbd5dcfb4c8605b9c78b784c765e437
Buwa1234 <140533991+Buwa1234@users.noreply.github.com>
2023-07-29 22:39:33 +0530

    Add files via upload

---
+++ Lab11/Lab11.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            AClass obj1 = new AClass();
+            int[] array = obj1.AMethod(size);
+
+            Console.WriteLine("\n Display the array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
