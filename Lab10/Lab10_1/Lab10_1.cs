f43248111b6cec56d81152b91280d8fe3cef3532
Buwa1234 <140533991+Buwa1234@users.noreply.github.com>
2023-07-29 22:34:17 +0530

    Add files via upload

---
+++ Lab10/Lab10_1/Lab10_1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter Value:");
+            	num[1] = int.Console.ReadLine();
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("num[1]");
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                sum = sum + num[1];
+            }
+            Console.ReadKey();
+        }
+    }
+}
